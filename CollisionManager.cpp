f68dbecbe817044ab9973b0d621430f3f702cbfb
Herovero <herovero0117@gmail.com>
2026-08-25 16:11:21 +0800

    Added raylib windows library

--- CollisionManager.cpp
+++ CollisionManager.cpp
@@ -2,7 +2,6 @@
 
 bool CollisionManager::CheckPlayerCollisions(const WauBulan& wau, const SwingingKid& kid, const ObstacleSpawner& spawner) {
     for (const auto& obs : spawner.obstacles) {
-        // obs->CheckCollision automatically routes to GapObstacle's special code if it is a Gap!
         if (obs->CheckCollision(wau.pos, wau.radius)) return true;
         if (obs->CheckCollision(kid.pos, kid.radius)) return true;
     }
